e3eb84e222ef5339ceb55da7f31776a8ba8c650e
namikerdogan <erdogannamik@gmail.com>
2025-11-18 20:57:56 +0300

    README ve pyproject.toml dosyaları eklendi; ana program kodu oluşturuldu.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import platform
 
 def main():
-    print("Hello from sipgiris!")
+    print("Hello from sipgiris !")
     print("Python sürümü:", platform.python_version())
     print("Detaylı sürüm (sys.version):", sys.version)
 
